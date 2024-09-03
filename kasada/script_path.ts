dd415240f9cb516d6faebc5b92bff2d4238da513
justhyped <justhyped@protonmail.com>
2024-09-03 16:15:17 +0200

    Fix `parseKasadaPath`

--- kasada/script_path.ts
+++ kasada/script_path.ts
@@ -1,10 +1,14 @@
 /**
  * Parses the Kasada ips.js path from the given HTML source code.
  * Returns `null` if the path can't be found.
+ * Replaces &amp; with & in the result.
  * @param src The HTML source code
  */
 export function parseKasadaPath(src: string): string | null {
     const scriptPathExpr = /<script\s+src="([^"]+)"/;
     const result = scriptPathExpr.exec(src);
-    return result?.[1] ?? null;
+    if (result?.[1]) {
+        return result[1].replace(/&amp;/g, '&');
+    }
+    return null;
 }
